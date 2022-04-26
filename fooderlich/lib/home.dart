7c92fa3ce042093f9d2f4984805812b910bc1a33
Sumith Damodaran <sumithpd@gmail.com>
2022-04-26 16:36:59 +0100

    adding fooderlich app

---
+++ fooderlich/lib/home.dart
@@ -0,0 +1,72 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+// 1
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // 7
+  int _selectedIndex = 0;
+
+// 8
+  static List<Widget> pages = <Widget>[
+    // TODO: Replace with Card1
+    Container(color: Colors.red),
+    // TODO: Replace with Card2
+    Container(color: Colors.green),
+    // TODO: Replace with Card3
+    Container(color: Colors.blue)
+  ];
+
+// 9
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          // 2
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: pages[_selectedIndex],
+
+      // 4
+      bottomNavigationBar: BottomNavigationBar(
+        // 5
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        // 6
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3',
+          ),
+        ],
+        // 10
+        currentIndex: _selectedIndex,
+// 11
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
